e31f90b6439b99504c0b188c4f7c5d935214ffb3
Mateus Farias <mateusfariasdev@outlook.com>
2023-08-25 01:19:51 -0300

    first commit

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { MessagesService } from './messages.service';
+
+@Controller('messages')
+export class MessagesController {
+  constructor(private messagesService: MessagesService) {}
+  @Get()
+  findAll() {
+    return this.messagesService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param() params) {
+    return this.messagesService.findById(+params.id);
+  }
+}
---
+++ src/messages/messages.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class MessagesService {
+  private messages = [
+    {
+      id: 1,
+      text: 'Primeira mensagem',
+    },
+    {
+      id: 2,
+      text: 'Segunda mensagem',
+    },
+  ];
+  findAll() {
+    return this.messages;
+  }
+
+  findById(id: number) {
+    return this.messages.find((message) => message.id === id);
+  }
+}
